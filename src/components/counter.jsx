0a22f1db4a8245163ac836391af1c7efc183072d
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-01 15:46:03 -0500

    Added function to our count logic

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,27 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class Counter extends Component{
-    state = {}
-    render() {
-        // jsx expression that gets compiled as React.createElement()
-        return <div><h1>Hello World</h1><button>Increment</button></div>;
-    }
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    // jsx expression that gets compiled as React.createElement()
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
 
+  // method to call a function to return a value
+  formatCount() {
+    // check the value of the count propery:
+    // return this.state.count === 0 ? "Zero" : this.state.count;
+
+    // improvement on code with object restructuring:
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 export default Counter;
