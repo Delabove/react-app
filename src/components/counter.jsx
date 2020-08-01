576154aa5958acc885214e47ec0bbbcf4c114273
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-01 15:59:00 -0500

    Removed styling to go to the next module

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,21 +5,12 @@ class Counter extends Component {
     count: 0,
   };
 
-  styles = {
-    fontSize: 30,
-    fontWeight: "bold",
-  };
-
   render() {
     // jsx expression that gets compiled as React.createElement()
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
-        <button style={{ fontSize: 30 }} className="btn btn-secondary btn-sm">
-          Increment
-        </button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
