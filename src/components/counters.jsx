19e3eb0b933d08942bdabcf62c094e2c62ca031b
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-02 13:58:28 -0500

    destructured the props and total counters property to simplify code

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,20 +3,19 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
     return (
       <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-small m-s"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-small m-s">
           Reset
         </button>{" "}
         <br />
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
